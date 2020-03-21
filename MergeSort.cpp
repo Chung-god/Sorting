0e11c84d832db1d876cae41c06205c3f61fbfad5
HyunChung Park <54928780+Chung-god@users.noreply.github.com>
2020-03-21 11:45:23 +0900

    Add files via upload

---
+++ MergeSort.cpp
@@ -0,0 +1,77 @@
+﻿// MergeSort.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void MergeTwoArea(int arr[], int left, int mid, int right) {
+	int fIdx = left;
+	int rIdx = mid + 1;
+	int i;
+
+	int* sortArr = (int*)malloc(sizeof(int) * (right + 1));
+	int sIdx = left;
+
+	while (fIdx <= mid && rIdx <= right) {
+		if (arr[fIdx] <= arr[rIdx])
+			sortArr[sIdx] = arr[fIdx++];
+		else
+			sortArr[sIdx] = arr[rIdx++];
+		sIdx++;
+	}
+
+	if (fIdx > mid) {
+		for (i = rIdx; i <= right; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+	else {
+		for (i = fIdx; i <= mid; i++, sIdx++)
+			sortArr[sIdx] = arr[i];
+	}
+
+	for (i = left; i <= right; i++)
+		arr[i] = sortArr[i];
+
+	free(sortArr);
+}
+
+void MergeSort(int arr[], int left, int right) {
+	int mid;
+	
+	if (left < right) {
+		//중간 지점을 계산한다.
+		mid = (left + right) / 2;
+
+		//둘로 나눠 정렬
+		MergeSort(arr, left, mid);//재귀, recursion
+		MergeSort(arr, mid + 1, right);
+
+		//정렬된 두 배열을 병합
+		MergeTwoArea(arr, left, mid, right);
+	}
+}
+int main()
+{
+	int arr[7] = { 3,2,4,1,7,6,5 };
+	int i;
+
+	//배열 arr의 전체 영역 정렬
+	MergeSort(arr, 0, sizeof(arr) / sizeof(int) - 1);
+
+	for (i = 0; i < sizeof(arr) / sizeof(int); i++)
+		printf("%d ", arr[i]);
+
+	printf("\n");
+	return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
